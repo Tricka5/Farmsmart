01a3ff6f82cad785fd91b3ce78e4645dc52645a2
“Mlinda24” <bsc-21-22@unima.ac.mw>
2024-11-12 23:14:56 +0200

    commit

---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login and Sign Up Page',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 219, 250, 224),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset(
+                  'Assets/Logo.png',
+                  height: 100,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+              ),
+              Text(
+                "Log into your Account",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                height: 40,
+                width: 400,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter email',
+                    hintStyle: TextStyle(color: Colors.black38),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                height: 40,
+                width: 400,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter password',
+                    hintStyle: TextStyle(color: Colors.black38),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.topCenter,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ResetPasswordPage()),
+                    );
+                  },
+                  child: Text(
+                    'FORGOT YOUR PASSWORD?',
+                    style: TextStyle(
+                        color: Colors.green[700], fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Login functionality
+                },
+                child: Text(
+                  'LOG IN',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      const Color.fromARGB(255, 23, 78, 27), // Deep green color
+                  minimumSize: Size(150, 50),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(color: Colors.green[700]),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 219, 250, 224),
+      appBar: AppBar(
+        title: Text('Sign Up or Log In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "enter full name",
+              ),
+              SizedBox(
+                  height: 100,
+                  width: 350,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'enter full name',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )),
+              SizedBox(height: 10),
+              Text("Email"),
+              SizedBox(
+                  height: 50,
+                  width: 350,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter email',
+                      hintStyle: TextStyle(color: Colors.black38),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )),
+              Text("password"),
+              SizedBox(height: 16),
+              SizedBox(
+                  height: 50,
+                  width: 350,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter password',
+                      hintStyle: TextStyle(color: Colors.black38),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[700],
+                  minimumSize: Size(200, 50), // Medium-sized button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ResetPasswordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[50],
+      appBar: AppBar(
+        title: Text('Reset Password'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your Email',
+                    hintText: 'Enter email',
+                    hintStyle: TextStyle(color: Colors.black38),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Reset password functionality
+                },
+                child: Text(
+                  'RESET PASSWORD',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[700],
+                  minimumSize: Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
