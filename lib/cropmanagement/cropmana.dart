9562b7f93a041cfe84b625d7553a549c9ae677bb
VChadzala <bed-com-12-21@unima.ac.mw>
2024-11-26 00:14:10 +0200

    another change

--- lib/cropmanagement/cropmana.dart
+++ lib/cropmanagement/cropmana.dart
@@ -4,7 +4,15 @@ void main() {
   runApp(MyApp());
 }
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CropManagementPage(),
+    );
+  }
+}
 
 class CropManagementPage extends StatefulWidget {
   @override
