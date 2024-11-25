7beebe9d54d0995a58e37c3fecd6846144758ebb
VChadzala <bed-com-12-21@unima.ac.mw>
2024-11-26 00:46:34 +0200

    new class

--- lib/cropmanagement/cropmana.dart
+++ lib/cropmanagement/cropmana.dart
@@ -121,14 +121,7 @@ class CropAnalyticsPage extends StatefulWidget {
   _CropAnalyticsPageState createState() => _CropAnalyticsPageState();
 }
 
-class _CropAnalyticsPageState extends State<CropAnalyticsPage> {
-  final _formKey = GlobalKey<FormState>();
-  Map<String, String> details = {
-    'Crop variety': '',
-    'Crop growth': '',
-    'Pest control': '',
-    'Harvestation': ''
-  };
+
 
   @override
   void initState() {
