b72513e68ae6e0df33671c4220f043bfff281bad
VChadzala <bed-com-12-21@unima.ac.mw>
2024-11-26 01:00:29 +0200

    crop change

--- lib/cropmanagement/cropmana.dart
+++ lib/cropmanagement/cropmana.dart
@@ -28,17 +28,7 @@ class _CropManagementPageState extends State<CropManagementPage> {
     });
   }
 
-  void deleteCrop(int index) {
-    setState(() {
-      savedDetails.removeAt(index);
-    });
-  }
-
-  void updateCrop(int index, Map<String, String> updatedDetails) {
-    setState(() {
-      savedDetails[index] = updatedDetails;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
